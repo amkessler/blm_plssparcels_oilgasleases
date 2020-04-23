aa5d44e042e7bbe847f7caf90d8967da2de16071
Aaron Kessler <kessler.aaron@gmail.com>
2020-04-23 13:10:05 -0400

    begin parsing the lease ld-summary column

--- 01_UT_joiningtoleases.R
+++ 01_UT_joiningtoleases.R
@@ -26,12 +26,39 @@ glimpse(firstdivisions)
 ### Now import the oil/gas lease data of proposed parcels ####
 lands_nominated_raw <- read_csv("lease_data/Lands_Nominated_Export_04_09_2020.csv")
 
-lands_nominated_raw %>% 
-  clean_names()
+lands_nominated <- lands_nominated_raw %>% 
+  clean_names() %>% 
+  mutate(
+    submitted_date = mdy(submitted_date)
+  )
+
+glimpse(lands_nominated)
+
+#the ld_summary column is the one with the township/range numbers
+#the challenge here is trying to match this up to the geo table columns
+lands_nominated %>% 
+  select(ld_summary)
+
+
+#let's parse the ld summary column into parts ####
+
+#what's the length of each entry, are they all the same?
+str_length(lands_nominated$ld_summary)
+
+#ah - all but the last one! There's an extraneous period in there
+lands_nominated %>% 
+  select(ld_summary) %>% 
+  tail(1)
+
+#let's take that out
 
 
 
-#using tmap
+
+
+
+
+#### MAPPING ####
 
 #map out just a portion of sections 
 ut_section_13 <- ut_firstdivisions_geo %>% 
